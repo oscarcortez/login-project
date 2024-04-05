6da995dd24de1fb8d4b72988efedc0349e7515c2
oscar-cortez-home <oscarkortez@gmail.com>
2024-04-05 17:29:07 -0400

    installed tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { FunctionComponent, ReactNode } from 'react'
 export const App: FunctionComponent = (): ReactNode => {
   return (
     <>
-      <h1>hello world</h1>
+      <h1 className="text-3xl font-bold underline">hello world</h1>
     </>
   )
 }
